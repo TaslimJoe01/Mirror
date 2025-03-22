306b36968aa504b0d297798b746423c2c5a5eb6c
TaslimJoe01 <162861113+TaslimJoe01@users.noreply.github.com>
2025-03-22 11:56:37 +0700

    -
    Signed-off-by: TaslimJoe01 <162861113+TaslimJoe01@users.noreply.github.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -123,14 +123,14 @@ def get_raw_file_size(size):
 
 def get_readable_file_size(size_in_bytes):
     if not size_in_bytes:
-        return "0B"
+        return "0 B"
 
     index = 0
     while size_in_bytes >= 1024 and index < len(SIZE_UNITS) - 1:
         size_in_bytes /= 1024
         index += 1
 
-    return f"{size_in_bytes:.2f}{SIZE_UNITS[index]}"
+    return f"{size_in_bytes:.2f} {SIZE_UNITS[index]}"
 
 
 def get_readable_time(seconds: int):
